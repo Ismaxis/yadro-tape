13aa6b89460f216a5fd0664b3c86030375f57ba4
Ismaxis <Ismaxis@github.com>
2024-05-20 23:59:49 +0300

    gflags added

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <gflags/gflags.h>
 #include <sort/tape_sort.h>
 #include <tape/file/file_tape.h>
 #include <util/bin_util.h>
@@ -8,13 +9,41 @@
 #include <fstream>
 #include <iostream>
 
-auto input = "D:/C++/yadro-tape/in.txt";
-auto ouptut = "D:/C++/yadro-tape/out.txt";
+DEFINE_string(input, "", "Path to input file");
+DEFINE_string(output, "", "Path to output file");
+DEFINE_bool(v, false, "Show info about tmp files and other");
 
-int main() {
+static bool is_empty_path(const char*, const std::string& path) {
+    return !path.empty();
+}
+
+DEFINE_validator(input, is_empty_path);
+DEFINE_validator(output, is_empty_path);
+
+void error(const std::string& message) {
+    std::cerr << "ERROR: " << message << std::endl;
+}
+
+int main(int argc, char** argv) {
+    gflags::SetUsageMessage(
+        "--input <input-file> --output <output-file> [-v]. Or enter --help to see help page");
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+    if (argc > 1) {
+        std::stringstream ss;
+        for (size_t i = 1; i < argc; ++i) {
+            ss << "'" << argv[i] << "' ";
+        }
+        error("Unknown argument(s): " + ss.str());
+        return -1;
+    }
+
+    std::string input = FLAGS_input;
+    std::string ouptut = FLAGS_output;
+
     auto bin_in_filename = file_util::new_tmp_file();
     auto bin_out_filename = file_util::new_tmp_file();
-    std::cout << bin_in_filename << ' ' << bin_out_filename << '\n';
+
     std::size_t n;
     {
         std::fstream in(input, std::fstream::in);
@@ -29,19 +58,32 @@ int main() {
             return -1;
         }
         n = bin_util::to_binary(in, bin_in);
-        std::cout << n << '\n';
+        if (FLAGS_v) {
+            std::cout << "Found " << n << " elements" << std::endl;
+        }
     }
 
+    std::cout << "Sorting..." << std::endl;
+
     {
+        if (FLAGS_v) {
+            std::cout << "Temp files:" << std::endl
+                      << "  input: " << bin_in_filename << std::endl
+                      << "  output: " << bin_out_filename << std::endl;
+        }
         file_tape in_tape(bin_in_filename);
         file_tape out_tape(bin_out_filename);
 
         tape_sort(in_tape, n, out_tape, [] {
-            return std::make_unique<file_tape>(file_util::new_tmp_file());
+            auto tmp = file_util::new_tmp_file();
+            if (FLAGS_v) {
+                std::cout << "  sorting tape: " << tmp << std::endl;
+            }
+            return std::make_unique<file_tape>(tmp);
         });
-
-        std::cout << "Sorted\n";
     }
+
+    std::cout << "Sorted" << std::endl;
 
     {
         std::fstream out(ouptut, std::fstream::out | std::fstream::trunc);
@@ -56,6 +98,6 @@ int main() {
             return -1;
         }
 
-        std::cout << bin_util::from_binary(bin_out, out, n) << '\n';
+        bin_util::from_binary(bin_out, out, n);
     }
 }
